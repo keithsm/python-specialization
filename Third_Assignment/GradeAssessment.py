43c71cac021a64bcf4bd317b31141bf48635bde1
Samuel M. Keith <SamuelMKeith@gmail.com>
2020-10-24 12:37:05 -0400

    Added comments for clarity.

--- Third_Assignment/GradeAssessment.py
+++ Third_Assignment/GradeAssessment.py
@@ -1,9 +1,12 @@
+#Enter and validate range of input (0.0 - 1.0).
 score = input("Enter Score (0.0 - 1.0): ")
 score = float(score)
 if score < 0.0 :
     print ("Score cannot be less than 0.0. Please retry.")
 elif score > 1.0 :
     print ("Score cannot be greater than 1.0). Please retry.")
+
+#Calculate and display grade
 elif score >= .9 : print ('A')
 elif score >= .8 : print ('B')
 elif score >= .7 : print ('C')
