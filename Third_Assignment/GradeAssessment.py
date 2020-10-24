3553a6e36e68745c8d9817bf17a18b183ff8c740
Samuel M. Keith <SamuelMKeith@gmail.com>
2020-10-24 12:23:31 -0400

    First version of the Grade Assessment program.

---
+++ Third_Assignment/GradeAssessment.py
@@ -0,0 +1,11 @@
+score = input("Enter Score (0.0 - 1.0): ")
+score = float(score)
+if score < 0.0
+    print ("Score cannot be less than 0.0. Please retry.")
+elseif score > 1.0
+    print ("Score cannot be greater than 1.0). Please retry.")
+elseif score < 0.6 : print ('F')
+elseif score >= .6 : print ('D')
+elseif score >= .7 : print ('C')
+elseif score >= .8 : print ('B')
+elseif score >= .9 : print ('A')
