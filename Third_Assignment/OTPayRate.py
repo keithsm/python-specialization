b0bf6ed391985384c3b9947175a98a316e8ea6da
Samuel M. Keith <SamuelMKeith@gmail.com>
2020-10-24 11:53:29 -0400

    Changed 'stpay' to 'regpay'.

--- Third_Assignment/OTPayRate.py
+++ Third_Assignment/OTPayRate.py
@@ -7,13 +7,13 @@ otrate = (1.5 * hrate)
 
 #Calculate and display base pay without overtime
 if hrs <= 40 :
-    stpay = (hrate * hrs)
+    regpay = (hrate * hrs)
     print (stpay)
 
 #Calculate and display first 40 hours at base hourly
 #rate and add overtime pay
 elif hrs > 40 :
-    stpay = (hrate * 40)
+    regpay = (hrate * 40)
     othrs = (hrs - 40)
     otpay = (othrs * otrate)
-    print (stpay + otpay)
+    print (regpay + otpay)
