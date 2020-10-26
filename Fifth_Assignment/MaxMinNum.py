53c0e3f9e3d11403d8c25b5cc028d352f402e019
Samuel M. Keith <SamuelMKeith@gmail.com>
2020-10-26 15:32:43 -0400

    Revised logic to correct low to high number entry.

--- Fifth_Assignment/MaxMinNum.py
+++ Fifth_Assignment/MaxMinNum.py
@@ -1,22 +1,37 @@
+#Declare that we don't have any existing values for
+#the largest and smallest numbers we're seeking.
 largest_num = None
 smallest_num = None
+
+#Define an infinite loop that will run until 'done'
+#is entered.
 while True:
-  num = input ('Enter a number: ')
-  if num == 'done' :
-    break
+  num = input ('Enter a number:')
+  if num == 'done' : break
   try :
-      num=float(num)
+      num=int(num)
   except :
       print('Invalid input')
       continue
-#Determine largest and smallest numbers
-  if largest_num is None :
-    largest_num = num
-  elif num > largest_num :
-    largest_num = num
-  elif smallest_num == None :
-    smallest_num = num
-  elif num < smallest_num :
-    smallest_num = num
+
+#After the first entry, largest_num and smallest_num
+#are equal
+  largest_num = num
+  smallest_num = num
+
+  if largest_num < num :
+        largest_num = num
+  elif smallest_num > num :
+        smallest_num = num
+
+#Determine the largest and smallest numbers
+#  if largest_num is None :
+#    largest_num = num
+#  elif num > largest_num :
+#    largest_num = num
+# elif smallest_num == None :
+#    smallest_num = num
+#  elif num < smallest_num :
+#    smallest_num = num
 print ('Maximum is', largest_num)
 print ('Minimum is', smallest_num)
