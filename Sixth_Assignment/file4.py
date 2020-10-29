7efa47a8c7fa3c89973e5d6077c258fd7a5abde3
Samuel M. Keith <SamuelMKeith@gmail.com>
2020-10-29 10:01:12 -0400

    modified file4.py with new text to elicit a merge conflict.

--- Sixth_Assignment/file4.py
+++ Sixth_Assignment/file4.py
@@ -1,2 +1,3 @@
 #This is a test file to highlight the branch funtionlality in Git
-#This file was created in Notepad in the 'even-newer-branch'.
+#This file was created in Notepad in the 'even-newer-branch' and I'm now working
+#to create a merge conflict to see how Git handles that kind of thing.
