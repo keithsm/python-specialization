39bcfa004d19cfd8b429a36f9e0f96df9c9c9743
Samuel M. Keith <SamuelMKeith@gmail.com>
2020-10-29 10:06:42 -0400

    Edited file4.py's text to create amerge conflict.  Let the games begin.

--- Sixth_Assignment/file4.py
+++ Sixth_Assignment/file4.py
@@ -1,2 +1,3 @@
 #This is a test file to highlight the branch funtionlality in Git
+#This text in the even-newer-branch should creat a conflict upon merge with the master.
 #This file was created in Notepad in the 'even-newer-branch'.
