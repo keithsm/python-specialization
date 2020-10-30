9e529eb4d81051da4e8b86ea4314866177d31a9f
Samuel M. Keith <SamuelMKeith@gmail.com>
2020-10-30 14:36:43 -0400

    Revised assignment number.

--- Eighth_Assignment/read_split.py
+++ Eighth_Assignment/read_split.py
@@ -1,4 +1,4 @@
-#Assignment 8.1
+#Assignment 8.4
 #Open the file romeo.txt and read it line by line.
 #For each line, split the line into a list of words
 #using the split() method. The program should build
@@ -7,14 +7,21 @@
 #not append it to the list. When the program completes,
 # sort and print the resulting words in alphabetical order.
 
+#Open romeo.txt file and read input
+#Open romeo.txt file and read input
 text = open('romeo.txt')
 input = text.read()
-#norm_text = input.lower()
+
+#Split the input and define an empty list
 words = input.split()
 word_list = list()
+
+#For each word determine if it's already in the
+#word_list. If not, add it.
 for item in words :
     if item not in word_list :
         word_list.append(item)
 
+#Sort and display
 word_list.sort()
 print(word_list)
